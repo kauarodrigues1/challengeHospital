f96cef405eafe2f95dd468c6bd0cf2ba68ba3981
Kaua <rodrigueskaua76@gmail.com>
2024-11-25 19:14:22 -0300

    Criando a estrutuda do index

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+let imagens =[
+    "src/assets/medico1.jpg",
+    "src/assets/medico2.jpg",
+    "src/assets/medico3.jpg",
+    
+];
+let i=0;
+let time =5000;
+
+function slideShow(){
+    document.getElementById("image").src=imagens[i];
+    i++;
+    if(i == imagens.length) {i=0;}
+    setTimeout("slideShow()",time)  
+    }
+    slideShow();
